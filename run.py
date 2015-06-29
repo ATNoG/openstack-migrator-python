9acd16f295d071309bb446cbc89d1e45cf2511d3
Rafael Ferreira <mail@rafaelferreira.pt>
2015-06-29 13:51:36 +0100

    compute list

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 from openstack import connection
+from openstack.profile import Profile
 import json
+
+# profile
+profile = Profile()
+profile.set_version('identity', 'v2')
 
 authentication_file = open('authentication.json', 'r')
 auth_args = json.load(authentication_file)
 
-conn = connection.Connection(**auth_args)
+conn = connection.Connection(profile=profile, **auth_args)
 
-projects = conn.identity.list_projects()
-
-pass
+for compute in conn.compute.servers():
+    print compute
